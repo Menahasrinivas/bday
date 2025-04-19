cf586c473cc987d6ad144bf38973d6ee4c0113b1
Menahasrinivas <menahasrinivas@gmail.com>
2025-04-20 00:30:01 +0530

    add

--- src/component/BirthdayWishes.jsx
+++ src/component/BirthdayWishes.jsx
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react';
 import './BirthdayWishes.css';
 import FireworksDisplay from './FireworksDisplay';
 
+const imageList = [
+  'public/image1.jpg',
+  '/image2.jpg',
+  '/image3.jpg',
+  '/image4.jpg'
+];
+
 function BirthdayWishes() {
   const [count, setCount] = useState(10);
   const [showWishes, setShowWishes] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  // Countdown logic
   useEffect(() => {
     const countdown = async () => {
       for (let i = 10; i > 0; i--) {
@@ -17,6 +26,19 @@ function BirthdayWishes() {
     countdown();
   }, []);
 
+  // Rotate images only after countdown finishes
+  useEffect(() => {
+    let interval;
+    if (showWishes) {
+      interval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) =>
+          prevIndex === imageList.length - 1 ? 0 : prevIndex + 1
+        );
+      }, 3000); // Change image every 3 seconds
+    }
+    return () => clearInterval(interval);
+  }, [showWishes]);
+
   return (
     <div className="main-container">
       {!showWishes ? (
@@ -24,8 +46,12 @@ function BirthdayWishes() {
       ) : (
         <>
           <div className="wishes-box">
-            <h1 className="wish-text">🎉 Happy Birthday lakshmi akka! 🎂💖</h1>
-            <img src="./public/image1.jpg" alt="Birthday" className="center-image" />
+            <h1 className="wish-text">🎉 Happy Birthday Lakshmi Akka! 🎂💖</h1>
+            <img
+              src={imageList[currentImageIndex]}
+              alt="Birthday"
+              className="center-image"
+            />
             <div className="confetti">💐🍫🎂🎈🎉</div>
           </div>
           <FireworksDisplay />
@@ -34,4 +60,34 @@ function BirthdayWishes() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default BirthdayWishes
