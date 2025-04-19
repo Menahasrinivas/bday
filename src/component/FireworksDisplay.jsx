40cc606b883b890e2fe567ca0eb9ae4d4d1bc55e
Menahasrinivas <menahasrinivas@gmail.com>
2025-04-20 00:54:00 +0530

    commit

--- src/component/FireworksDisplay.jsx
+++ src/component/FireworksDisplay.jsx
@@ -7,6 +7,20 @@ const FireworksDisplay = () => {
     useEffect(() => {
       const container = ref.current;
       if (!container) return;
+      <canvas
+  ref={canvasRef}
+  className="fireworks-canvas"
+  style={{
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    zIndex: 1, // canvas stays behind image
+    pointerEvents: 'none',
+  }}
+></canvas>
+
   
       const fireworks = new Fireworks(container, {
         speed: 2,
